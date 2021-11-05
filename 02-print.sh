de046c9d9ff52bc1546f600a4288e4d60f83c63b
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-05 11:30:21 +0530

    write some print.sh commands

---
+++ 02-print.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## To print some text on screen then we can use echo command or printf command
+## we choose to go with echo command because of its less syntaxing
+
+# syntax
+# echo message to print
+
+echo hello world
+echo welcome
+
