8cdc2d7f01aec528d1a10ca64c22062f8f5cc94a
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-07 18:29:17 +0530

    Added some lines and tabs and colours

--- 02-print.sh
+++ 02-print.sh
@@ -19,3 +19,23 @@ echo welcome
 
 # echo -e "word1\t\tword2"
 
+# coloured output
+# syntax echo -e "\e[COLmMessage"
+
+## colours        CODE
+# Red               31
+# Green             32
+# Yellow            33
+# Blue              34
+# magenta           35
+# cyan              36
+
+# echo -e "\e[31mText in Red colour"
+# echo -e "\e[32mText in Green colour"
+# echo -e "\e[33mText in Yellow colour"
+# echo -e "\e[34mText in Blue colour"
+# echo -e "\e[35mText in Magenta colour"
+# echo -e "\e[36mText in Cyan colour"
+
+
+
