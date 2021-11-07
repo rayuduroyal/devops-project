f939a554d31d1e4634b7e592f99a490702a54a29
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-07 21:07:25 +0530

    Added variables

---
+++ 03-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## if we assign a name to a set of data , then that is a variable
+# syntax VAR=DATA
+
+# number
+a=100
+# string
+b=abc
+
+# in bash shell and also wby default there are no data types, shell considers everything as string
+
+# access the data in shell using $ charecter prifixing the variable name
+
+# echo value of a=$a
+# echo value of b=$b
