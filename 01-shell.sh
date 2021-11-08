8d1d418e7a05a6402feb7ba76fe4cbf6a3154bfa
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-08 16:00:06 +0530

    Added shell.sh

--- 01-shell.sh
+++ 01-shell.sh
@@ -3,9 +3,3 @@
 # #! is called as she-bang, it denotes the path of interpreter and also ensure the script executes with that interpreter if you execute the script with ,/script.sh instead of bash script.sh
 
 # Any line starts with # is a comment and those lines will be ignored during execution
-
-
-
-
-
-
