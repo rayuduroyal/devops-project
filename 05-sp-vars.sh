d6acbce7838dbfdf81b0def0a1af3ff8f56076a1
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-08 20:51:53 +0530

    Added sp-vars.sh

--- 05-sp-vars.sh
+++ 05-sp-vars.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# 0 is script name
 # echo 0 = $0
+# first argument to script
 # echo 1 = $1
+# second argument to script
 # echo 2 = $2
-# echo 3 = $3
-# echo 4 = $4
 
+## * and @ are giving all the argument
 # echo "* = $*"
 # echo "@ = $@"
+
+# number of values which are parsed
 # echo "# = $#"
+
+# echo -e "your name=$1\your age =$2"
