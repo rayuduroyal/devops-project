4566f0b510fb839eedef8b5c94383c369877a560
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-15 21:37:17 +0530

    added create-instance.sh

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
- aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t3.micro
+ aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
