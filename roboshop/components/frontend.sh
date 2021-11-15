b66fed47e34ca1eed0745a239843a5dfdbd3ac1d
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-15 16:15:15 +0530

    added frontend.sh

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -27,7 +27,7 @@ unzip -d /usr/share/nginx/html /tmp/frontend.zip &>>$Log
 stat $?
 
 print "copy files to nginx path"
-mv /tmp/frontend-main/static/* /usr/share/nginx/html/. &>>$Log
+mv /tmp/frontend-main/static/* /usr/share/nginx/html/.
 stat $?
 
 mv static/* .
