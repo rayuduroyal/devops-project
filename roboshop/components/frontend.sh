ecf3b986fd7d1fea010cc6a5122464d40a125006
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-15 11:10:24 +0530

    added if condition

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -10,7 +10,7 @@ rm -rf $Log
 
 print "installing nginx"
 yum install nginx -y &>>$Log
-if [ $? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[1;32mSUCCESS\e[0m"
 else
   echo -e "\e[1;31mFAILURE\e[0m"
