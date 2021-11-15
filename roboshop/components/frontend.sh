e12f2209f514c708d15de1128a955247f21feb5d
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-15 15:49:56 +0530

    added make file

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -14,8 +14,11 @@ print "starting nginx"
 systemctl start nginx &>>$Log
 stat $?
 
+print "download Html pages"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$Log
+stat $?
+
 exit
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
