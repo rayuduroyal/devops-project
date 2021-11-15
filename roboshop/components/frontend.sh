9a7f74bf4b082d64478f4c1231ead6b9947477b2
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-15 14:55:42 +0530

    added frontend.sh

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
-print() {
-  echo -n -e "\e[1m$1\e[0m ..."
-  echo -e "\n\e[36m==================== $1 ====================\e[0m" >>$Log
-  }
-
-stat() {
-  if [ $1 -eq 0 ]; then
-    echo -e "\e[1;32mSUCCESS\e[0m"
-  else
-    echo -e "\e[1;31mFAILURE\e[0m"
-  fi
-}
-
-Log=/tmp/roboshop.log
-rm -rf $Log
-
-print "installing nginx"
-yum install nginx -y &>>$Log
-stat $?
-
+source components/common.sh
 
 print "enabling nginx"
 systemctl enable nginx
@@ -31,7 +12,6 @@ stat $?
 
 exit
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
