dfe7707dee21199fc95156598d4c26c573eb9064
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-15 10:29:49 +0530

    added if condition

--- 08-if.sh
+++ 08-if.sh
@@ -2,8 +2,14 @@
 
 read -p 'enter username: ' username
 
-if [ "$username" == "root" ];then
+if [ "$username" == "root" ]; then
   echo "hey, you are root user"
 else
   echo Hey, you are nonroot user
+fi
+
+if [ $UID -eq 0 ]; then
+  echo you are a root use
+else
+   echo you are nonroot user
 fi
