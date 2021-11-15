593d0ef6f1eb147547c246df0126500713c617cb
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-15 22:04:55 +0530

    added create-instance.sh

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+ COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=cart" | jq ".Reservations[].Instances[].InstanceId" | wc -l)
 
- aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+if [ $COUNT -eq 0 ]; then
+   aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+else
+  echo "instance already exists"
+fi
+
