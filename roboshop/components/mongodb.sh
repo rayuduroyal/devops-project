13586f3252006acd451927292e9b6877a031dc55
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-16 13:05:51 +0530

    Added Mongodb.sh

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -3,37 +3,37 @@
 source components/common.sh
 
 print "download repo"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo. &>>$LOG
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo. &>>$Log
 stat $?
 
 print "Install MongoDB"
-yum install -y mongodb-org &>>$LOG
+yum install -y mongodb-org &>>$Log
 stat $?
 
 print "Update MongoDB Config"
-sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$Log
 stat $?
 
 print "Start MongoDB"
-systemctl Restart mongod &>>$LOG
+systemctl Restart mongod &>>$Log
 stat $?
 
 print "Enabling MongoDB"
-syste#mctl enable mongod &>>$LOG
+syste#mctl enable mongod &>>$Log
 stat $?
 
 print "Download schema"
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$Log
 stat $?
 
 print "Extract Schema"
-Unzip -o -d /tmp mongodb.zip &>>$LOG
+Unzip -o -d /tmp mongodb.zip &>>$Log
 stat $?
 
 print "Load schema"
 cd /tmp/mongodb-main
-mongo < catalogue.js &>>$LOG
-mongo < users.js. &>>$LOG
+mongo < catalogue.js &>>$Log
+mongo < users.js. &>>$Log
 stat $?
 
 
