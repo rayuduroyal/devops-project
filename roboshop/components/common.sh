aefdf6d75f820132ca588982ef388f70bfdb9c98
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-18 13:26:35 +0530

    Added common.sh

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,24 +1,23 @@
-print() {
+Print() {
   LSPACE=$(echo $1 | awk '{print length}')
-  SPACE=$(($MSPACE-$MSPACE))
+  SPACE=$(($MSPACE-$LSPACE))
+  SPACES=""
   while [ $SPACE -gt 0 ]; do
     SPACES="$SPACES$(echo ' ')"
-    $SPACE=$(($SPACE-1))
+    SPACE=$(($SPACE-1))
   done
-  echo $SPACES | cat -A
-  echo -n -e "\e[1m$1\e[0m ..."
-  echo -e "\n\e[36m==================== $1 ====================\e[0m" >>$Log
-  }
+  echo -n -e "\e[1m$1${SPACES}\e[0m  ... "
+  echo -e "\n\e[36m======================== $1 ========================\e[0m" >>$LOG
+}
 
-stat() {
+Stat() {
   if [ $1 -eq 0 ]; then
     echo -e "\e[1;32mSUCCESS\e[0m"
   else
     echo -e "\e[1;31mFAILURE\e[0m"
-    echo -e "\e[1;33mscript failed and check the detailed log in $Log file\e[0m "
+    echo -e "\e[1;33mScript Failed and check the detailed log in $LOG file\e[0m"
     exit 1
   fi
 }
-
 Log=/tmp/roboshop.log
 rm -rf $Log
