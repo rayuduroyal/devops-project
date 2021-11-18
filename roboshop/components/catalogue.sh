b681c2cc075f43c71d1045436ab54d085855b6d6
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-18 16:30:26 +0530

    Updated catalogue.sh

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,57 +1,13 @@
 #!/bin/bash
 
 source components/common.sh
-MSPACE=$(cat $0 | grep ^print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
-print "Install NodeJS"
-yum install nodejs make gcc-c++ -y &>>$Log
-stat $?
+MSPACE=$(cat $0 components/common.sh | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
-print "Add RoboShop User"
-id roboshop &>>$Log
-if [ $? -eq 0 ]; then
-  echo User Roboshop already exists &>>$Log
-else
-  useradd roboshop &>>$Log
-fi
-stat $?
 
-print "Download Catalogue"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$Log
-stat $?
-
-print "Remove Old content"
-rm -rf /home/roboshop/catalogue &>>$Log
-stat $?
-
-print "Extract Catalogue"
-unzip -o -d  /home/roboshop /tmp/catalogue.zip &>>$Log
-stat $?
-
-print "Copy Content"
-mv /home/roboshop/catalogue-main /home/roboshop/catalogue &>>$Log
-stat $?
-
-print "Install NodeJS Dependencies"
-cd /home/roboshop/catalogue
-npm install --unsafe-perm &>>$Log
-stat $?
-
-print "Fix App Permissions"
-chown -R roboshop:roboshop /home/roboshop &>>$Log
-stat $?
-
-print "Update Dns records in SystemD Config"
-set -i -e 's/MONGO DNSNAME/mongodb.roboshop.internal'/home/roboshop/catalogue/systemd.service &>>$Log
-stat $?
-
-print "Copy SystemD File"
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$Log
-stat $?
-
-print "Start Catalogue Service"
-systemctl daemon-reload &>>$Log && systemctl restart catalogue &>>$Log && systemctl enable catalogue &>>$Log
-stat $?
+COMPONENT_NAME=Catalogue
+COMPONENT=catalogue
+NODEJS
 
 print "Cheking DB Conections From APP"
 sleep 5
