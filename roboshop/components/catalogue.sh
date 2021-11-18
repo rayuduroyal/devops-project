72029b0cb77d89579e371152ff8b51f9b61baa9b
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-18 17:06:35 +0530

    Updated catalogue.sh

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -6,15 +6,7 @@ MSPACE=$(cat $0 components/common.sh | grep ^Print | awk -F '"' '{print $2}' | a
 
 COMPONENT_NAME=Catalogue
 COMPONENT=catalogue
-NODEJS
 
-print "Cheking DB Conections From APP"
-sleep 5
-STAT=$(curl -s localhost:8080/health | jq .mongo)
-echo status = $STAT
-if [ "$STAT" == "true" ]; then
-  stat 0
-else
-  stat 1
-fi
+NODEJS
+CHECK_MONGO_FROM_APP
 
