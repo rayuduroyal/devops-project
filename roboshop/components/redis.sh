3d1dd8db0436b21c393b7d7dffd32427685ba239
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-18 15:13:55 +0530

    Updated redis.sh

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
 source components/common.sh
-MSPACE=$(cat $0 | grep ^print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+MSPACE=$(cat $0 | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
-print "Install Redis Repos"
-yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$Log
-stat $?
+Print "Install Redis Repos"
+yum install yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$Log
+Stat $?
 
-print "Enable Redis Repos"
+Print "Enable Redis Repos"
 yum-config-manager --enable remi &>>$Log
-stat $?
+Stat $?
 
-print "Install Redis"
+Print "Install Redis"
 yum install redis -y &>>$Log
-stat $?
+Stat $?
 
 
-print "Update Redis Listen Address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>$Log
-stat $?
+Print "Update Redis Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>$Log
+Stat $?
 
 
-print "Start Redis Database"
-systemctl enable redis &>>$Log && systemctl start redis &>>$Log
-stat $?
-
+Print "Start Redis Database"
+systemctl enable redis &>>$LOG && systemctl restart redis &>>$Log
+Stat $?
 
