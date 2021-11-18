119d48e74a27f064631a8421f068fa14d44e9a0b
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-18 15:33:06 +0530

    Updated create-instance.sh

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
- COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null wc -l)
+CREATE() {
+  COUNT=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | wc -l)
 
-if [ $COUNT -eq 0 ]; then
-   aws ec2 run-instances --image-id ami-0855cab4944392d0a --instance-type t3.micro --security-group-ids sg-000e3d77edcfc8e1f --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
-else
-  echo "instance already exists"
-fi
+  if [ $COUNT -eq 0 ]; then
+    aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t3.micro --security-group-ids sg-000e3d77edcfc8e1f --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
+  else
+    echo -e "\e[1;33m$1 Instance already exists\e[0m"
+    return
+  fi
 
-IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
-## xargs is used to remove the double quotes
-sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id Z061860719ROPUM6YV8RG --change-batch file:///tmp/record.json | jq
+  sleep 5
+
+  IP=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
+  ## xargs is used to remove the double  quotes
+  sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z061860719ROPUM6YV8RG --change-batch file:///tmp/record.json | jq  &>/dev/null
