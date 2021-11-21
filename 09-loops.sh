a732689c169257adfa32ed01586254b058b5b0ef
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-21 16:05:56 +0530

    Updated shell scripting

---
+++ 09-loops.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Loops are two major commands , while & for
+
+# While loop marks on Expressions that we used in if statements
+
+a=10
+while [ $a -gt 0 ]; do
+  echo while Loop
+  sleep
+  a=$(($a-1))
+done
+
