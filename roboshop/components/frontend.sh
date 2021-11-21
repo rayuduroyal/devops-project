b92044b433930b303365cdd02e996fdc3dbd6024
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-21 12:44:34 +0530

    Updated shell scripting

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,42 +4,42 @@ source components/common.sh
 MSPACE=$(cat $0 | grep ^print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
 print "installing nginx"
-yum install nginx -y &>>$Log
+yum install nginx -y &>>$LOG
 stat $?
 
 print "enabling nginx"
-systemctl enable nginx &>>$Log
+systemctl enable nginx &>>$LOG
 stat $?
 
 print "starting nginx"
-systemctl start nginx &>>$Log
+systemctl start nginx &>>$LOG
 stat $?
 
 print "download Html pages"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$Log
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 stat $?
 
 print "remove old Html pages"
-rm -rf /usr/share/nginx/html/* &>>$Log
+rm -rf /usr/share/nginx/html/* &>>$LOG
 stat $?
 
 print "extract frontend archive"
-unzip -o -d /tmp /tmp/frontend.zip &>>$Log
+unzip -o -d /tmp /tmp/frontend.zip &>>$LOG
 stat $?
 
 print "copy files to nginx path"
-mv /tmp/frontend-main/static/* /usr/share/nginx/html/. &>>$Log
+mv /tmp/frontend-main/static/* /usr/share/nginx/html/. &>>$LOG
 stat $?
 
 print "copy Nginx Roboshop config file"
-cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$Log
+cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 stat $?
 
 print "Enabling nginx"
-systemctl enable nginx &>>$Log
+systemctl enable nginx &>>$LOG
 stat $?
 
 print "starting nginx"
-systemctl start nginx &>>$Log
+systemctl start nginx &>>$LOG
 stat $?
 
