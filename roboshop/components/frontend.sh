af57a9a951c549c0219529401da776130ee270b1
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-21 12:46:01 +0530

    Updated shell scripting

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,45 +1,43 @@
 #!/bin/bash
 
 source components/common.sh
-MSPACE=$(cat $0 | grep ^print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+MSPACE=$(cat $0 | grep Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
-print "installing nginx"
+
+Print "Installing Nginx"
 yum install nginx -y &>>$LOG
-stat $?
+Stat $?
 
-print "enabling nginx"
-systemctl enable nginx &>>$LOG
-stat $?
-
-print "starting nginx"
-systemctl start nginx &>>$LOG
-stat $?
-
-print "download Html pages"
+Print "Download Html Pages"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
-stat $?
+Stat $?
 
-print "remove old Html pages"
+Print "Remove Old Html Pages"
 rm -rf /usr/share/nginx/html/* &>>$LOG
-stat $?
+Stat $?
 
-print "extract frontend archive"
+Print "Extract Frontend Archive"
 unzip -o -d /tmp /tmp/frontend.zip &>>$LOG
-stat $?
+Stat $?
 
-print "copy files to nginx path"
+Print "Copy files to Nginx path"
 mv /tmp/frontend-main/static/* /usr/share/nginx/html/. &>>$LOG
-stat $?
+Stat $?
 
-print "copy Nginx Roboshop config file"
-cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
-stat $?
+Print "Copy Nginx Roboshop Config file"
+cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf  &>>$LOG
+Stat $?
 
-print "Enabling nginx"
+Print "Update Nginx Config file"
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/'  -e '/user/ s/localhost/user.roboshop.internal/'  -e '/payment/ s/localhost/payment.roboshop.internal/'  -e '/shipping/ s/localhost/shipping.roboshop.internal/'   /etc/nginx/default.d/roboshop.conf  &>>$LOG
+Stat $?
+
+Print "Enabling Nginx"
 systemctl enable nginx &>>$LOG
-stat $?
+Stat $?
 
-print "starting nginx"
-systemctl start nginx &>>$LOG
-stat $?
+Print "Starting Nginx"
+systemctl restart nginx &>>$LOG
+Stat $?
+
 
