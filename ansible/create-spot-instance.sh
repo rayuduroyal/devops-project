10dc417fb475e274f2bd599d34eef820e2d261ba
susarlarayudu <rayuduroyal1996@gmail.com>
2021-11-28 19:58:38 +0530

    Updated ansible

--- ansible/create-spot-instance.sh
+++ ansible/create-spot-instance.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+UPDATE_DNS_RECORDS() {
+    IP=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
+    ## xargs is used to remove the double  quotes
+    sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
+    aws route53 change-resource-record-sets --hosted-zone-id Z0641400MP3ISPG0RSOK --change-batch file:///tmp/record.json | jq  &>/dev/null
+}
+
 CREATE() {
   COUNT=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | wc -l)
 
   if [ $COUNT -eq 0 ]; then
-    aws ec2 run-instances --launch-template LaunchTemplateId=lt-0db19f164a02a8ff6,Version=2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
+    aws ec2 run-instances --launch-template LaunchTemplateId=lt-0db19f164a02a8ff6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
   else
     echo -e "\e[1;33m$1 Instance already exists\e[0m"
+    UPDATE_DNS_RECORDS $1
     return
   fi
 
   sleep 5
 
-  IP=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
-  ## xargs is used to remove the double  quotes
-  sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z0641400MP3ISPG0RSOK --change-batch file:///tmp/record.json | jq  &>/dev/null
+  UPDATE_DNS_RECORDS $1
 }
 
 if [ "$1" == "all" ]; then
@@ -27,4 +32,3 @@ if [ "$1" == "all" ]; then
 else
   CREATE $1
 fi
-
