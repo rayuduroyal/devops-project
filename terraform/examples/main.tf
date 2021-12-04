a0852b7b10a7a7004f7e8a9f3640870af193b86c
susarlarayudu <rayuduroyal1996@gmail.com>
2021-12-04 19:59:00 +0530

    Updated ansible

--- terraform/examples/main.tf
+++ terraform/examples/main.tf
@@ -24,9 +24,7 @@ resource "aws_security_group" "allow_sample" {
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
+
     }
   ]
 
@@ -38,9 +36,6 @@ resource "aws_security_group" "allow_sample" {
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
     }
   ]
 
