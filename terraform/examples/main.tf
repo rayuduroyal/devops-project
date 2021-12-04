ddb82824b2bd02f6126e9c97b244c607d5521d6d
susarlarayudu <rayuduroyal1996@gmail.com>
2021-12-04 18:57:51 +0530

    Updated ansible

--- terraform/examples/main.tf
+++ terraform/examples/main.tf
@@ -1,3 +1,43 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_security_group" "allow_sample" {
+  name        = "allow_sample"
+  description = "Allow sample traffic"
+
+  ingress = [
+    {
+      description      = "TLS from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+    }
+  ]
+
+  egress = [
+    {
+      description      = "egress"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+
+  tags = {
+    Name = "allow_sample"
+  }
+}
+
+resource "aws_instance" "sample" {
+  ami           = ""
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
