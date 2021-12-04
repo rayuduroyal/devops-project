e7c387901714cb18a8f48b116a252801dc27c131
susarlarayudu <rayuduroyal1996@gmail.com>
2021-12-04 19:22:00 +0530

    Updated ansible

--- terraform/examples/main.tf
+++ terraform/examples/main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_instance" "sample" {
+  ami           = "ami-0855cab4944392d0a"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_sample.id, "sg-000e3d77edcfc8e1f"]
+
+  tags = {
+    Name = "sample"
+  }
+}
+
 resource "aws_security_group" "allow_sample" {
   name        = "allow_sample"
   description = "Allow sample traffic"
@@ -34,10 +44,11 @@ resource "aws_security_group" "allow_sample" {
 }
 
 resource "aws_instance" "sample" {
-  ami           = ""
+  ami           = "ami-0855cab4944392d0a"
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_sample.id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "sample"
   }
 }
