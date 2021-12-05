be4c7b1e5058d30cf4c7178bb842e50dc1201186
susarlarayudu <rayuduroyal1996@gmail.com>
2021-12-05 16:49:13 +0530

    Updated ansible

--- terraform/examples/ec2/main.tf
+++ terraform/examples/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "sample" {
   ami           = "ami-0855cab4944392d0a"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_sample.id, "sg-000e3d77edcfc8e1f"]
+  vpc_security_group_ids = []
 
   tags = {
     Name = "sample"
--- terraform/examples/main.tf
+++ terraform/examples/main.tf
@@ -2,47 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_security_group" "allow_sample" {
-  name        = "allow_sample"
-  description = "Allow sample traffic"
-
-  ingress = [
-    {
-      description      = "TLS from VPC"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
-
-  egress = [
-    {
-      description      = "egress"
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
-
-  tags = {
-    Name = "allow_sample"
-  }
-}
-
 terraform {
   backend "s3" {
     bucket = "terraform.b60"
     key    = "example/sample/terraform.tfstate"
     region = "us-east-1"
   }
+}
+
+module "ec2" {
+  source = "./ec2"
+}
+
+module "sg" {
+  source = "./sg"
 }
