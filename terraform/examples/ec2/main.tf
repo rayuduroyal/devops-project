0d9a385f0829960749c59d3ad55d85a5309f74f8
susarlarayudu <rayuduroyal1996@gmail.com>
2021-12-06 11:06:36 +0530

    Updated terraform

--- terraform/examples/ec2/main.tf
+++ terraform/examples/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "sample" {
   count                  = var.env == "prod" ? 1: 0
-  ami                    = ami-0855cab4944392d0a
+  ami                    = "ami-0855cab4944392d0a"
   instance_type          = var.instance_type == "" ? "t3.micro" : var.instance_type
   vpc_security_group_ids = [var.SGID]
 
